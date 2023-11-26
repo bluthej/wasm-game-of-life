13096c9ce29e5e92956e21d8bc2e3fbf56ed0dd7
bluthej <joffrey.bluthe@e.email>
2023-11-26 11:15:55 +0100

    Add test and convenience methods

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -37,6 +37,20 @@ impl Universe {
         (row * self.width + column) as usize
     }
 
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
         for delta_row in [self.height - 1, 0, 1] {
@@ -87,6 +101,22 @@ impl Universe {
         self.cells.as_slice().as_ptr()
     }
 
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = FixedBitSet::with_capacity((width * self.height) as usize);
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = FixedBitSet::with_capacity((self.width * height) as usize);
+    }
+
     pub fn add_glider(&mut self, row: u32, col: u32) {
         for delta_row in [self.height - 2, self.height - 1, 0, 1, 2] {
             for delta_col in [self.width - 2, self.width - 1, 0, 1, 2] {
@@ -97,18 +127,13 @@ impl Universe {
             }
         }
 
-        for (delta_row, delta_col) in [
+        self.set_cells(&[
             (self.height - 1, 1),
             (0, self.width - 1),
             (0, 1),
             (1, 0),
             (1, 1),
-        ] {
-            let row = (row + delta_row) % self.height;
-            let col = (col + delta_col) % self.width;
-            let idx = self.get_index(row, col);
-            self.cells.set(idx, true);
-        }
+        ]);
     }
 
     pub fn tick(&mut self) {
